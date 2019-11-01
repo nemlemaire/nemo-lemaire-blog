88a4878c397b5df817ae1faae84fb657f6a72790
nemlemaire <nemo.lemaire@student.umons.ac.be>
2019-11-01 14:40:41 +0100

    fin du vocabulaire

--- docs/posts/2019-10-29-article-2/image_texte/texte article 2.R
+++ docs/posts/2019-10-29-article-2/image_texte/texte article 2.R
@@ -4,9 +4,9 @@ The material, developed by researchers from the University of Cambridge, is made
 
 In nature, animals such as chameleons and **cuttlefish** are able to change colour thanks to **chromatophores**: skin cells with contractile fibres that move **pigments** around. The pigments are spread out to show their colour, or squeezed together to make the cell clear.
 
-The artificial chromatophores developed by the Cambridge researchers are built on the same principle, but instead of contractile fibres, their colour-changing abilities **rely on** light-powered nano-mechanisms, and the 'cells' are microscopic drops of water.
+The artificial chromatophores developed by the Cambridge researchers are built on the same principle, but instead of contractile fibres, their colour-changing abilities rely on light-powered nano-mechanisms, and the 'cells' are microscopic drops of water.
 
-When the material is heated above 32C, the nanoparticles store large amounts of **elastic energy** in a fraction of a second, as the polymer coatings expel all the water and **collapse**. This has the effect of forcing the nanoparticles **to bind** together into tight **clusters**. When the material is cooled, the polymers take on water and expand, and the gold nanoparticles are strongly and quickly pushed apart, like a spring.
+When the material is heated above 32C, the nanoparticles store large amounts of **elastic energy** in a fraction of a second, as the polymer coatings expel all the water and collapse. This has the effect of forcing the nanoparticles **to bind** together into tight **clusters**. When the material is cooled, the polymers take on water and expand, and the gold nanoparticles are strongly and quickly pushed apart, like a spring.
 
 "**Loading** the nanoparticles into the microdroplets allows us to control the shape and size of the clusters, giving us dramatic colour changes," said Dr Andrew Salmon from Cambridge's Cavendish Laboratory, the study's co-first author.
 
@@ -14,9 +14,9 @@ The geometry of the nanoparticles when they bind into clusters determines which 
 
 At the moment, the material developed by the Cambridge researchers is in a single **layer**, so is only able to change to a single colour. However, different nanoparticle materials and shapes could be used in extra layers to make a fully dynamic material, like real chameleon skin.
 
-The researchers also observed that the artificial cells can 'swim' in simple ways, similar to the algae Volvox. Shining a light on one **edge** of the droplets causes the surface **to peel towards** the light, pushing it forward. Under stronger **illumination**, high pressure bubbles briefly form to push the droplets along a surface.
+The researchers also observed that the artificial cells can 'swim' in simple ways, similar to the algae Volvox. Shining a light on one **edge** of the droplets causes the surface **to peel** towards the light, pushing it forward. Under stronger **illumination**, high pressure bubbles briefly form to push the droplets along a surface.
 
-"This work is a big advance in using **nanoscale** technology to do biomimicry," said co-author Sean Cormier. "We're now working to replicate this on **roll-to-roll films** so that we can make metres of colour changing sheets. Using structured light we also plan to use the light-triggered swimming to '**herd**' droplets. It will be really exciting to see what collective **behaviours** are generated."
+"This work is a big advance in using **nanoscale** technology to do biomimicry," said co-author Sean Cormier. "We're now working to replicate this on **roll-to-roll films** so that we can make metres of colour changing sheets. Using structured light we also plan to use the light-triggered swimming to '**herd**' droplets. It will be really exciting to see what collective behaviours are generated."
 
 The research was **funded** by the European Research Council (ERC) and the Engineering and Physical Sciences Research Council (EPSRC).
 
