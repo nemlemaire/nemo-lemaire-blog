a79895d97abe69e993384bd889e03fe179856b89
nemlemaire <nemo.lemaire@student.umons.ac.be>
2019-11-10 21:41:02 +0100

    article 3

---
+++ _posts/2019-11-10-article-3/article 3 texte.R
@@ -0,0 +1,39 @@
+A team of engineers and marine biologists built a better **suction cup** inspired by the mechanism that allows the **clingfish** to adhere to both smooth and rough surfaces, such as rocks in the area where the tide comes and goes.
+
+Researchers **reverse engineered** the clingfish's suction disk and developed **devices** that **cling** well to wet and dry objects both in an out of water. The devices can hold up to hundreds of times their own weight. They could be used in a wide range of applications from handling and packaging for produce, to robotic **grippers** in manufacturing, to the recovery of archaeological **artifacts**.
+
+The University of California San Diego team presents their findings in a recent issue of the journal Bioinspiration and Biomimetics.
+
+The **overarching** goal of the project was to develop a new technology capable of providing delicate grip to handle fragile objects. The study highlights the importance of biomimicry, which allows scientists across disciplines from engineering to biology to work together and take inspiration from nature to develop new technologies.
+
+"I have always been fascinated by nature, and specifically the **intricate** and **outright** fascinating designs that have evolved and that can do what modern technology cannot," said Jessica Sandoval, a Ph.D. student at the UC San Diego Jacobs School of Engineering and lead author of the study. "This was a perfect opportunity to get to study the adhesion mechanism in these clingfish and address how the technology behind adhesion from suction cups can be improved."
+
+Improving suction cups through biomimicry
+
+Clingfish are small fish widespread in tropical and temperate regions. They are common in **intertidal** areas, where they use their powerful suction ability to adhere to rocks, algae, and seagrass. They can remain stuck to these surfaces even in powerful currents and when **battered** by waves. The clingfish used in this study were a species native to the West Coast, and were collected right off San Diego.
+
+By studying the clingfish they collected, Sandoval and colleagues found that the secret mimicking the attachment mechanism that the animal uses was to incorporate a soft layer and **slits** in the artificial suction cups. The clingfish's suction disc is lined with rows of hexagonal structures, called **papillae**, which are covered with microscopic fibers. Researchers mimicked this inside their prototypes with a soft layer made out of silicone. That layer dramatically improved adhesive performance on rough surfaces. The team also incorporated slits into the chamber of the suction cup, which insures better adhesion to irregular, concave surfaces.
+
+"When first speaking with my engineering colleagues, I was sure the trick to optimize the suction cup would come from biology," said study coauthor Dimitri Deheyn, a researcher in marine biology and biomimicry at the Scripps Institution of Oceanography at UC San Diego. "I also was sure a better suction cup would combine a unique design architecture and a 3D, versatile mechanism of some sort."
+
+Suction cup performance
+
+The artificial suction discs researchers developed were able to stick to rough surfaces, such as **coarse sand papers**, and to highly variable surfaces, from rocks to vegetables, both in and out of water. Researchers also showed that their devices could pick up everything from cherries and strawberries without smashing them, to elaborate **conch** shells and vases.
+
+"Many adhesive devices adhere well only to either a dry or a wet surface and have difficulties with rough surfaces," said study coauthor Mike Tolley, a robotics expert and professor at the Jacobs School of Engineering at UC San Diego. "Our devices can do it all."
+
+The devices can hold onto a heavy object for over six hours, which researchers believe could be extended to longer periods of time. In addition, these clingfish-inspired suction discs have an impressive grip given their size: a suction disc can support up to 350 times its own weight while suspended in the air.
+
+The researchers even equipped the arm of a **Remotely Operated Vehicle** (ROV) with one of their devices and showed it could manipulate a raw egg without breaking it.
+
+"This specific ROV application is of particular interest to me," said Sandoval, who is a deep sea ROV pilot in addition to being a Ph.D. student. "While piloting ROVs, I use subsea manipulators in order to recover delicate **samples** from the sea floor. I often wish I had a tool for delicate grip to complement the powerful hold of the metallic jaws of the manipulator. My work in the deep sea was a true motivation behind looking to nature for inspiration for adhesion."
+
+Experiments and analysis
+
+The clingfish were collected at the tide pools just north of the Ellen Browning Scripps Pier. Part of the Scripps Coastal Marine Reserve, scientists at UC San Diego can acquire licenses to collect organisms for scientific purposes from this area, making it a unique **asset** for the university.
+
+While building clingfish-inspired suction discs, the team also experimented with mimicking the microscopic fibers of the clingfish by coating the **footprint** of the artificial suction disc with a layer of microscopic pillars made of silicone. Interestingly, they found that the simpler designs that consisted of just a thick layer of silicone performed better than designs with microscopic pillars.
+
+The interdisciplinary team analyzed the performance of the clingfish-inspired artificial suction discs. The researchers characterized the surface contact of the footprint of the discs to see how they interact with a surface. They sought to understand the **underlying** energetics by using **Finite Element Analysis** to model the process of deformation of the suction discs. They also performed tests of adhesive strength on different surface curvatures and different surface roughness, ranging from smooth acrylic to coarse sand paper. They tested the amount of force needed to attach the disc to a surface, depending on surface roughness. They found that very little force was needed to attach the discs to a surface which explains the devices' ability to handle delicate objects.
+
+This study is supported by the Office of Naval Research grant number N000141712062. Sandoval is supported by the Gates Millennium Scholars program. This work was facilitated by the Biomimicry for Emerging Science and Technology Initiative (BEST initiative;
