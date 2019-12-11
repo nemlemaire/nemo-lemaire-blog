044a7e8efc587052eadd475579883d64402d071a
nemlemaire <nemo.lemaire@student.umons.ac.be>
2019-12-11 18:58:54 +0100

    article 5 fin

--- _posts/2019-11-25-article-5/article 5 text.R
+++ _posts/2019-11-25-article-5/article 5 text.R
@@ -1,17 +1,17 @@
 Achieving strength and **extensibility** at the same time has so far been a great challenge in material engineering: increasing strength has meant losing extensibility and vice versa. Now Aalto University and VTT Technical Research Centre of Finland researchers have succeeded in **overcoming** this challenge, inspired by nature.
 
-The researchers created a truly new bio-based material by gluing together wood **cellulose fibres** and the **silk** protein found in **spider web** **threads**. The result is a very firm and **resilient** material which could be used in the future as a possible replacement for plastic, as part of bio-based **composites** and in medical applications, surgical fibres, textile industry and packaging.
+The researchers created a truly new bio-based material by gluing together wood **cellulose** fibres and the **silk** protein found in **spider web** **threads**. The result is a very firm and **resilient** material which could be used in the future as a possible replacement for plastic, as part of bio-based **composites** and in medical applications, surgical fibres, textile industry and packaging.
 
 According to Aalto University Professor Markus Linder, nature offers great ingredients for developing new materials, such as firm and easily **available** cellulose and tough and flexible silk used in this research. The advantage with both of these materials is that, unlike plastic, they are **biodegradable** and do not damage nature the same way **micro-plastic** do.
 
 "Our researchers just need to be able to reproduce these natural **properties**," adds Linder, who was also leading the research.
 
-"We used **birch tree pulp**, broke it down to cellulose nanofibrils and aligned them into a stiff **scaffold**. At the same time, we infiltrated the cellulosic **network** with a soft and energy dissipating spider silk adhesive **matrix**," says Research Scientist Pezhman Mohammadi from VTT.
+"We used **birch tree** pulp, broke it down to cellulose nanofibrils and aligned them into a stiff **scaffold**. At the same time, we infiltrated the cellulosic **network** with a soft and energy dissipating spider silk adhesive **matrix**," says Research Scientist Pezhman Mohammadi from VTT.
 
 Silk is a natural protein which is excreted by animals like silkworms and also found in spider web threads. The spider web silk used by Aalto University researchers, however, is not actually taken from spider webs but is instead produced by the researchers using bacteria with synthetic **DNA**.
 
 "Because we know the structure of the DNA, we can copy it and use this to manufacture silk protein molecules which are chemically similar to those found in spider web threads. The DNA has all this information contained in it," Linder explains.
 
-"Our work illustrates the new and **versatile** possibilities of protein engineering. In future, we could manufacture similar composites with slightly different **building blocks** and achieve a different set of characteristics for other applications. Currently we are working on making new composite materials as implants, impact resistance objects and other products," says Pezhman.
+"Our work illustrates the new and **versatile** possibilities of protein engineering. In future, we could manufacture similar composites with slightly different building blocks and achieve a different set of characteristics for other applications. Currently we are working on making new composite materials as implants, impact resistance objects and other products," says Pezhman.
 
 The research project is part of the work of the the Centre of Excellence in Molecular Engineering of Biosynthetic Hybrid Materials (Hyber).
