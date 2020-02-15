4cd741ca10e11f04775bf7b66a20bc4636c88f89
nemlemaire <nemo.lemaire@student.umons.ac.be>
2020-02-15 15:55:46 +0100

    fin article 6 + modification des catégories

--- _posts/2020-02-09-article-6/article 6 text.R
+++ _posts/2020-02-09-article-6/article 6 text.R
@@ -1,22 +1,22 @@
 Engineers have developed a new scanning technique inspired by the natural world that can detect **corroding** metals in oil and gas **pipelines**.
 
-By mimicking how bats use differing **wavelengths** of ultrasound to detect objects, hunt, and avoid predators, engineers have developed a new system that combines two separate types of radiation, fast **neutrons** and **gamma rays**, to detect corrosion—a major cause of pipeline **leaks**.
+By mimicking how bats use differing **wavelengths** of ultrasound to detect objects, hunt, and avoid predators, engineers have developed a new system that combines two separate types of radiation, fast neutrons and gamma rays, to detect corrosion—a major cause of pipeline **leaks**.
 
-With thousands of kilometers of pipelines used to transport oil and gas over huge distances globally, leaks are a major **issue** costing millions annually and have the potential to cause accidents and injuries as well as significant environmental damage.
+With thousands of kilometers of pipelines used to transport oil and gas over huge distances globally, leaks are a major issue costing millions annually and have the potential to cause accidents and injuries as well as significant environmental damage.
 
-Typically, corrosion in oil pipelines is measured with ultrasonic or electromagnetic techniques. However, these are not practical for underground pipelines, or for pipelines covered with insulating layers of **concrete** or plastic.
+Typically, corrosion in oil pipelines is measured with ultrasonic or electromagnetic techniques. However, these are not practical for underground pipelines, or for pipelines covered with insulating layers of concrete or plastic.
 
 The new system, developed by Engineers from Lancaster University, the National Physical Laboratory, and a technology company, Hybrid Instruments Ltd, exploits the reflected signals, known as :**backscatter**," of a combination of isolated fast-neutron and gamma radiation.
 
-Neutrons and gamma rays have useful complementary characteristics. Neutrons interact mainly with low-**density** materials like plastics. In addition, fast neutrons have a high penetrating power, so they are **suitable** for **probing** thick materials. Gamma rays interact mainly with metals and not always are able to penetrate very thick materials of high density.
+Neutrons and gamma rays have useful complementary characteristics. Neutrons interact mainly with low-density materials like plastics. In addition, fast neutrons have a high penetrating power, so they are suitable for **probing** thick materials. Gamma rays interact mainly with metals and not always are able to penetrate very thick materials of high density.
 
 The two radiation types produce a different electronic signal. This means researchers can **retain** data on both types of radiation simultaneously using a novel detecting device called a "Mixed Field Analyser," previously developed by Lancaster University and Hybrid Instruments Ltd.
 
 The system produces a pencil-like **beam** of probing radiation, of neutrons and gamma, which is directed at the steel section being inspected.
 
-The team tested the two imaging techniques in real time in a laboratory on **samples** of carbon-steel of different thicknesses.
+The team tested the two imaging techniques in real time in a laboratory on samples of carbon-steel of different thicknesses.
 
-The researchers were able to see differences in steel thickness. The sensors also worked when an insulating layer was replicated, with concrete or plastic, indicating the **likelihood** that **defects** in steels, as well as corrosion and **rust**, would produce variations in the backscatter.
+The researchers were able to see differences in steel thickness. The sensors also worked when an insulating layer was replicated, with concrete or plastic, indicating the likelihood that **defects** in steels, as well as corrosion and rust, would produce variations in the backscatter.
 
 These results indicate that if used on real pipelines then potential issues could be more easily detected and resolved before oil and gas is able to escape.
 
@@ -24,7 +24,7 @@ These results indicate that if used on real pipelines then potential issues coul
 
 "This system works a bit like the **chirps** made by bats. These chirps are a superposition of different ultrasound wavelengths, which bounce back to the bats' ears. As well as highlighting the benefits of combining multiple reflection sensing techniques to detect for problems such as corrosion, our work further illustrates the significant potential that can be had from taking inspiration from, and mimicking, systems that have evolved in the natural world."
 
-"Isolating neutrons and gamma rays backscattered from a steel surface in real time, in a way **analogous** to the way bats' brains isolate backscatter ultrasound and thus avoid confusion with their own chirps, could help us isolate **flaws** in pipe walls more quickly and effectively," said Professor Malcolm Joyce of Lancaster University and Hybrid Instruments Ltd.
+"Isolating neutrons and gamma rays backscattered from a steel surface in real time, in a way **analogous** to the way bats' brains isolate backscatter ultrasound and thus avoid confusion with their own chirps, could help us isolate flaws in pipe walls more quickly and effectively," said Professor Malcolm Joyce of Lancaster University and Hybrid Instruments Ltd.
 
 "This is a great example of NPL's world-leading neutron facilities being used for innovative science with a positive impact," said Neil Roberts of the National Physical Laboratory.
 
@@ -32,4 +32,4 @@ The intention is that the detector system would be further developed and used to
 
 The researchers suggest the technology could also be used in other applications, such as inspecting the integrity of structures such as bridges.
 
-The research has been **outlined in the paper** "Depicting corrosion-born defects in pipelines with combined neutron/γ ray backscatter: a biometric approach," which has been published by the journal Scientific Reports.
+The research has been outlined in the paper "Depicting corrosion-born defects in pipelines with combined neutron/γ ray backscatter: a biometric approach," which has been published by the journal Scientific Reports.
